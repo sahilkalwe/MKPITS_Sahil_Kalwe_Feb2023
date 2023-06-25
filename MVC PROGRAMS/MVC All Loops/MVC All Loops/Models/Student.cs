97c681b0e4e98248226fe5939697da76d98cf976
sahilkalwe <sahilkalwe@gmail.com>
2023-06-25 07:08:27 +0530

    25 june added

---
+++ MVC PROGRAMS/MVC All Loops/MVC All Loops/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace MVC_All_Loops.Models
+{
+    public class Student
+    {
+
+        public string name;
+        public int age;
+        //constructor
+
+        public Student(string a, int b)
+        {
+            name = a;
+            age = b;
+        }
+        //creating property named Name and Age
+        public string Name
+        {
+
+            get
+            {
+                return name;
+
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+    }
+}
---
+++ MVC PROGRAMS/MVC website Making 24-6-23/MVC website Making 24-6-23/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_website_Making_24_6_23.Models;
+using System.Diagnostics;
+
+namespace MVC_website_Making_24_6_23.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Insights()
+        {
+            return View();
+        }
+        public IActionResult Careers()
+        {
+            return View();
+        }
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
